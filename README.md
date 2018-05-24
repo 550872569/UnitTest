4db549e4aee6ca812a58339e52b671070bdd4741
yanhangpu <sogou@sogoudeiMac.local>
2018-05-24 15:55:32 +0800

    查看代码覆盖率

--- README.md
+++ README.md
@@ -1,2 +1,27 @@
 # UnitTest
 UnitTest
+
+```
+1. 单元测试的目的是为了测试数据合法性
+eg:
+    创建一个数据模型，属性 age name
+    age 数据区间 0~120；
+    name 不能为空，
+    
+2. 引入模型类头文件到UnitTest文件
+3. 创建一个test 开头的函数用于测试
+4. 在test 函数中用XCTAssert 测试数据合法性
+eg：
+    XCTAssert(obj.name != nil, @"姓名为空");
+    如果 obj.name 数据为空，会报错 到这一行，说明有一个输入源数据name 为空，向上查找即可
+    XCTAssert(obj.age > 0 && obj.age < 130, @"年龄超限");
+    如果输入源的age 131 那么断言 就会报错 年龄超限 如果 断言为true 那么不报错
+
+5. 点击刚才创建的test开头函数的左边有一个run按钮开始测试
+
+
+XCTAssert 想当于 XCTAssertTrue  如果条件为！那么报错
+XCTAssertFalse 相反 如果条件为！ 不报错 否则报错
+XCTAssertNotNil 条件不能为NIl 不报错，否则报错
+
+```
--- UnitTest/UnitTestTests/UnitTestTests.m
+++ UnitTest/UnitTestTests/UnitTestTests.m
@@ -31,26 +31,36 @@
 }
 
 - (void)testNewObj {
-    [self checkObjInitDict:@{@"age":@12,@"name":@"lilei"}];
-    [self checkObjInitDict:@{@"age":@12,@"name":@"      "}];
-    //    [self checkObjInitDict:@{@"age":@131,@"name":@"hanmeimei"}];
+//    [self checkObjInitDict:@{@"age":@12,@"name":@"lilei"}];//合法数据
+    [self checkObjInitDict:@{@"age":@12,@"name":@"      "}];//XCTAssert(![obj.name containsString:@"  "], @"姓名不合法");//如果数据不包含空格 那么数据合法，否则不合法
+    
+//    [self checkObjInitDict:@{@"age":@12,@"name":@""}];//XCTAssert(obj.name != nil, @"姓名为空");//判断数据不能为空 否则报错
+    
+    
+//    [self checkObjInitDict:@{@"age":@131,@"name":@"hanmeimei"}];//XCTAssert(obj.age > 0 && obj.age < 130, @"年龄超限");//判断数据为 0-130 否则报错
 }
+
+/**
+ 创建一个数据模型，属性 age name
+ age 数据区间 0~120；
+ name 不能为空，
+ */
 - (void)checkObjInitDict:(NSDictionary *)dict {
-    NSString *name = dict[@"name"];
+    NSString *name = dict[@"name"];// 获取数据源 ，和结果做对比
     NSInteger age = [dict[@"age"] integerValue];
-    TestObj *obj = [TestObj objWithDict:dict];
-    
-    XCTAssert(obj.name == nil, @"姓名为空");
-    XCTAssert([name isEqualToString:obj.name], @"姓名不一致");
-    
-    if (obj.age > 0 && obj.age < 130) {
-        XCTAssert(age == obj.age, @"年龄不一致");
-    } else {
-        XCTAssert(obj.age == 0, @"年龄超限");
-    }
+    TestObj *obj = [TestObj objWithDict:dict];//实际要测试的方法
+    TestObj *obj1;//对象为初始化 为空对象
+    XCTAssert(obj.name != nil, @"姓名为空");//判断数据不能为空 否则报错
+    XCTAssert([name isEqualToString:obj.name], @"姓名不一致");//判断数据一致性 否则报错 防止objWithDict 内部出错
+    XCTAssert(age == obj.age, @"年龄不一致");//判断数据一致性 否则报错 防止objWithDict 内部出错
+    XCTAssert(obj.age > 0 && obj.age < 130, @"年龄超限");//判断数据为 0-130 否则报错
+//    XCTAssert(![obj.name containsString:@"  "], @"姓名不合法");//如果数据不包含空格 那么数据合法，否则不合法
+//    XCTAssertTrue(![obj.name containsString:@"  "], @"姓名不合法");
+//    XCTAssertFalse([obj.name containsString:@"  "], @"姓名不合法");
+
+//    XCTAssertNotNil(obj1);//判断不为空，为空报错
     
     NSLog(@"obj:%@",obj);
-    XCTAssertNotNil(obj);
 }
 
 - (void)testPerformanceExample {
